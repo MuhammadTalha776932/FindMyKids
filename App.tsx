cc9c99f6b1daddd9fbe655a154986ada38ff5a3b
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-19 19:30:38 +0500

    init the FindMyKids Project

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+
+} from 'react-native';
+
+
+
+function App() {
+
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
