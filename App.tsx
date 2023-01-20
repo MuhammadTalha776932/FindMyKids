7af72242ed931ec77f542b9dc7e041c441771f1a
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-20 06:47:44 +0500

    Added the Splash-Screen
    Added the Splash Screen using react-native-splash-screen. Edits the android/app/build.gradle + android/settings.gradle + MainApplication.java + MainActivity.java in java folder of android

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import SplashScreen from 'react-native-splash-screen';
 import {
   SafeAreaView,
 
@@ -8,7 +8,13 @@ import LaunchPage from './component/index';
 
 
 
-function App(){
+function App():JSX.Element{
+
+  React.useEffect(()=>{
+    
+    const unSplashScreen:void = SplashScreen.hide();
+    return ()=> unSplashScreen
+  },[])
 
   return (
     <SafeAreaView>
