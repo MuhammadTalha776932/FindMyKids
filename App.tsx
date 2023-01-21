023a3b97e7ba27db6acad1ea1bde975864445b08
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-21 23:19:17 +0500

    added the googel maps

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React,{useEffect} from "react";
 import SplashScreen from "react-native-splash-screen";
-import {SafeAreaView,View} from "react-native";
-import ParentRegister from "./component/ParentRegister";
+import {SafeAreaView,View,StyleSheet} from "react-native";
+// import ParentRegister from "./component/ParentRegister";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: "100%",
+    width: "100%",
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
 
 function App(){
   useEffect(()=>{
@@ -12,9 +25,19 @@ function App(){
   },[])
 
   return (
-    <SafeAreaView>
-        <ParentRegister/>
-    </SafeAreaView>
+    <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    >
+    </MapView>
+  </View>
   );
 }
 
