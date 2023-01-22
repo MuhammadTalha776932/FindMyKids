c3bc5f5d6ce7afcc1c42709f67cc1ba227dd77e4
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-22 10:12:55 +0500

    Added the default marker in maps

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useRef} from "react";
 import SplashScreen from "react-native-splash-screen";
 import {SafeAreaView,View,StyleSheet} from "react-native";
 // import ParentRegister from "./component/ParentRegister";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE,Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import { GOOGLE_MAPS_APIKEY } from "./APIs_Keys";
 
@@ -36,6 +36,7 @@ function App(){
     }
   })
 
+  const mapsRef = useRef(null);
   const {initCords,finalCords} = state;
 
   useEffect(()=>{
@@ -47,22 +48,30 @@ function App(){
   return (
     <View style={styles.container}>
     <MapView
+      ref={mapsRef}
       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
       style={styles.map}
-      region={{      
-        latitude: 30.7046,
-        longitude: 76.7179,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }}
-
+      region={initCords}
     >
+      <Marker title="Start" description="Starting Points" coordinate={initCords}/>
+      <Marker title="Destinations" description="Finishing Points"  coordinate={finalCords}/>
     <MapViewDirections
     origin={initCords}
     destination={finalCords}
     apikey={GOOGLE_MAPS_APIKEY}
     strokeWidth={3}
     strokeColor="hotpink"
+    optimizeWaypoints={true}
+    onReady={result =>{
+      mapsRef?.current?.fitToCoordinates(result.coordinates,{
+        edgePadding:{
+          right:30,
+          top:300,
+          left:30,
+          bottom:100
+        }
+      });
+    }}
   />
     </MapView>
   </View>
